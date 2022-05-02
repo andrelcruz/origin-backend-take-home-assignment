bf47f02ece7f1b2098e5a964b1ccf71ee5da6bdc
andreLC <andre.cruz@talentmatching.com.br>
2022-05-01 23:45:20 -0300

    feat: use config port on api setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './AppModule'
+import { EnvironmentHelper } from './core/config/EnvironmentHelper'
 import { ValidationExceptionFilter } from './core/filters'
 import { CustomClassValidatorValidationPipe } from './core/pipes'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const CONFIG = EnvironmentHelper.getConfigAsObject()
+  const { PORT } = CONFIG.API
 
+  const app = await NestFactory.create(AppModule)
   app.useGlobalFilters(new ValidationExceptionFilter())
 
   app.useGlobalPipes(new CustomClassValidatorValidationPipe())
-  await app.listen(3000)
+  await app.listen(PORT)
 }
 bootstrap()
